9ab8d59ae9aa7f8422921ed3cee342887c1cb42f
Darren B <68653294+Devd0@users.noreply.github.com>
2023-04-24 18:14:32 -0400

    formatting and 401s

--- examples/actix.rs
+++ examples/actix.rs
@@ -1,24 +1,23 @@
 use actix_web::{web, App, HttpServer, Responder};
-use clerk_rs::{ClerkConfiguration, validators::actix::ClerkMiddleware};
+use clerk_rs::{validators::actix::ClerkMiddleware, ClerkConfiguration};
 
 async fn index() -> impl Responder {
-    "Hello world!"
+	"Hello world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    HttpServer::new(|| {
-        let config = ClerkConfiguration::new(None, None, Some("sk_test_key".to_string()), None);
-        App::new().service(
-            // prefixes all resources and routes attached to it...
-            web::scope("/app")
-                .wrap(ClerkMiddleware::new(config))
-                // ...so this handles requests for `GET /app/index.html`
-                .route("/index", web::get().to(index)),
-        )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+	HttpServer::new(|| {
+		let config = ClerkConfiguration::new(None, None, Some("sk_test_key".to_string()), None);
+		App::new().service(
+			// prefixes all resources and routes attached to it...
+			web::scope("/app")
+				.wrap(ClerkMiddleware::new(config))
+				// ...so this handles requests for `GET /app/index.html`
+				.route("/index", web::get().to(index)),
+		)
+	})
+	.bind(("127.0.0.1", 8080))?
+	.run()
+	.await
 }
--- examples/api.rs
+++ examples/api.rs
@@ -1,17 +1,16 @@
+use clerk_rs::{apis::emails_api::Email, clerk::Clerk, models::CreateEmailRequest, ClerkConfiguration};
 use tokio;
-use clerk_rs::{clerk::Clerk, ClerkConfiguration, models::CreateEmailRequest, apis::emails_api::Email};
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create a new clerk configuration so that we can make authed requests
-    let config = ClerkConfiguration::new(None, None, Some("sk_test_key".to_string()), None);
-    // Initialize our Clerk client with the newly created configuration
-    let client = Clerk::new(config);
-    // Create a new email using the Email api...
-    let new_email = CreateEmailRequest::new();
+	// Create a new clerk configuration so that we can make authed requests
+	let config = ClerkConfiguration::new(None, None, Some("sk_test_key".to_string()), None);
+	// Initialize our Clerk client with the newly created configuration
+	let client = Clerk::new(config);
+	// Create a new email using the Email api...
+	let new_email = CreateEmailRequest::new();
 
-    let _ = Email::create(&client, Some(new_email)).await;
+	let _ = Email::create(&client, Some(new_email)).await;
 
-    Ok(())
+	Ok(())
 }
