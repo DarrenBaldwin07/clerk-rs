04609c2b11a1064ca6a1fddc35413d40728faf45
Lev Khoroshansky <sololev27@gmail.com>
2023-12-20 23:49:30 +0100

    feat: Add Twitter OAuth strategy

--- src/models/email_address_verification.rs
+++ src/models/email_address_verification.rs
@@ -48,12 +48,14 @@ impl Default for Status {
 pub enum Strategy {
 	#[serde(rename = "admin")]
 	Admin,
-	#[serde(rename = "from_oauth_google")]
-	FromOAuthGoogle,
 	#[serde(rename = "phone_code")]
 	PhoneCode,
 	#[serde(rename = "email_code")]
 	EmailCode,
+	#[serde(rename = "from_oauth_google")]
+	FromOAuthGoogle,
+	#[serde(rename = "from_oauth_twitter")]
+	FromOAuthTwitter,
 	#[serde(other)]
 	Other,
 }
