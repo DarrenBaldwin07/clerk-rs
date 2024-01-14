5a29ea2a407feb6c70bc526f0ce8c9163fda3109
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-14 17:31:36 -0500

    add Other fallback for email verification strategy

--- src/models/email_address_verification.rs
+++ src/models/email_address_verification.rs
@@ -44,10 +44,11 @@ impl Default for Status {
 	}
 }
 ///
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum Strategy {
 	#[serde(rename = "admin")]
 	Admin,
+	Other(String)
 }
 
 impl Default for Strategy {
