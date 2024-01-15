70d173b75b55c5b81346ef51c38fca2c0ad4c70e
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-15 09:35:31 -0500

    typo

--- src/apis/organization_memberships_api.rs
+++ src/apis/organization_memberships_api.rs
@@ -63,9 +63,9 @@ pub enum UpdateOrganizationMembershipMetadataError {
 	UnknownValue(serde_json::Value),
 }
 
-pub struct OragnizationMebership;
+pub struct OrganizationMebership;
 
-impl OragnizationMebership {
+impl OrganizationMebership {
 	/// Adds a user as a member to the given organization. Only users in the same instance as the organization can be added as members.
 	pub async fn create(
 		clerk_client: &Clerk,
