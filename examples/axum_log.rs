d82cd488da8e9c60c8e6bc5ffd477511b78138ca
Owen Bartolf <owen.bartolf@Gmail.com>
2025-04-26 18:09:40 -0600

    Refactored example to be more like the other refactored example.

--- examples/axum_log.rs
+++ examples/axum_log.rs
@@ -10,17 +10,18 @@ use clerk_rs::{
 	ClerkConfiguration,
 };
 
+/// This is an unprotected route. 
 async fn index() -> &'static str {
 	"Hello world! This is a public route that requires no authentication."
 }
 
+/// This is a protected route. You can grab the currently authed user's
+/// ClerkJwt using an Extension extractor like so. Make sure you only
+/// use this extractor on protected routes, or else you will get a
+/// runtime error.
 async fn profile(Extension(clerk_jwt): Extension<ClerkJwt>) -> String {
 	format!(
-		"Hello, {}! This is an example of a protected route.\n
-		The Clerk-rs Axum middleware validates the user's\n
-		token. If valid, it attaches the JWT as an Axum \n
-		Extension, which can then be retrieved using the \n
-		Extension Extractor.",
+		"Hello, {}! This is an example of a protected route.",
 		clerk_jwt.sub
  	)
 }
@@ -37,10 +38,12 @@ async fn main() -> std::io::Result<()> {
 	let clerk = Clerk::new(config);
 
 	let app = Router::new()
-        .route("/", get(index))
-        .route("/index", get(index))
+		.route("/", get(index))
+		.route("/index", get(index))
 		.route("/profile",  get(profile))
 		.layer(ClerkLayer::new(MemoryCacheJwksProvider::new(clerk), Some(vec![String::from("/profile")]), true));
+		// ^^ The second argument of the ClerkLayer constructor is for flagging which routes should be protected
+		//    by the middleware and which should not be protected. If None is provided, all routes are protected 
 
 	let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
 	axum::serve(listener, app).await
